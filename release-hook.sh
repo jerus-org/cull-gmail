d7832cfbaa7000c34af9ade6337f21d5d6c52b85
Jeremiah Russell <jerry@jrussell.ie>
2025-09-30 07:01:32 +0100

    🔧 chore(scripts): update release hook script
    - change readme file path for consistency
    - replace cull-gmail with gen-changelog for changelog generation

--- release-hook.sh
+++ release-hook.sh
@@ -3,9 +3,9 @@
 # Build an updated README
 cat docs/readme/head.md > README.md
 # shellcheck disable=SC2129
-cat docs/readme/lib.md >> README.md
+cat docs/lib.md >> README.md
 cat docs/main.md >> README.md
 cat docs/readme/tail.md >> README.md   
 
 # Build Changelog
-cull-gmail generate --display-summaries --next-version "$SEMVER"
+gen-changelog generate --display-summaries --next-version "$SEMVER"
