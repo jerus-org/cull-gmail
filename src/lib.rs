b11ea689e10cf9d3ce8a4b5a993777211d008b4f
Jeremiah Russell <jerry@jrussell.ie>
2025-10-01 10:04:15 +0100

    ✨ feat(core): add client and credential modules
    - introduce client module for API interactions
    - implement credential module for authentication

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,11 @@
 #![cfg_attr(docsrs, feature(rustdoc_missing_doc_code_examples))]
 #![cfg_attr(docsrs, warn(rustdoc::invalid_codeblock_attributes))]
 #![doc = include_str!("../docs/lib.md")]
+
+mod client;
+mod credential;
+mod list;
+
+pub use client::get_auth;
+pub use client::get_client;
+pub use credential::Credential;
