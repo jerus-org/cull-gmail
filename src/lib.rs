ab8df6bdd981866483fb05ecb513b9cd34a1c1ed
Jeremiah Russell <jerry@jrussell.ie>
2025-10-07 15:51:21 +0100

    ✨ feat(lib): add config and retention modules
    - add config module for managing application settings
    - add retention module for message age management

--- src/lib.rs
+++ src/lib.rs
@@ -4,16 +4,23 @@
 #![cfg_attr(docsrs, warn(rustdoc::invalid_codeblock_attributes))]
 #![doc = include_str!("../docs/lib.md")]
 
+mod config;
 mod credential;
+mod eol_cmd;
 mod error;
 mod labels;
 mod message_list;
+mod retention;
 mod trash;
+
 pub(crate) mod utils;
 
+pub use config::Config;
 pub use credential::Credential;
 pub use error::Error;
 pub use labels::Labels;
 pub use message_list::DEFAULT_MAX_RESULTS;
 pub use message_list::MessageList;
+pub use retention::MessageAge;
+pub use retention::Retention;
 pub use trash::Trash;
