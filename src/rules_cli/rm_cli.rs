16145f8b2270eb294e512af84eb9fa5b561ab38d
Jeremiah Russell <jerry@jrussell.ie>
2025-10-08 16:15:08 +0100

    ✨ feat(rules_cli): implement rule removal
    - add `rm` subcommand to remove rules by id or label
    - add `id` and `label` options to `rm` subcommand
    - implement `remove_rule_by_id` and `remove_rule_by_label` in `Config`

--- src/rules_cli/rm_cli.rs
+++ src/rules_cli/rm_cli.rs
@@ -2,10 +2,29 @@ use clap::Parser;
 use cull_gmail::{Config, Error};
 
 #[derive(Debug, Parser)]
-pub struct RmCli {}
+pub struct RmCli {
+    /// Id of the rule to remove
+    #[clap(short, long)]
+    id: Option<usize>,
+    /// Label in the rule to remove (the rule will be removed)
+    #[clap(short, long)]
+    label: Option<String>,
+}
 
 impl RmCli {
-    pub fn run(&self, _config: Config) -> Result<(), Error> {
+    pub fn run(&self, mut config: Config) -> Result<(), Error> {
+        if self.id.is_none() && self.label.is_none() {
+            return Err(Error::NoRuleSelector);
+        }
+
+        if let Some(id) = self.id {
+            config.remove_rule_by_id(id)?;
+        }
+
+        if let Some(label) = &self.label {
+            config.remove_rule_by_label(label)?;
+        }
+
         Ok(())
     }
 }
