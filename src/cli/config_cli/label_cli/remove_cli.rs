efe4df5c50b5693d0226db6b5ffee5a6d74599ec
Jeremiah Russell <jerry@jrussell.ie>
2025-10-09 14:40:35 +0100

    ✨ feat(cli): add remove label subcommand
    - add `remove-label` subcommand to remove labels from rules

---
+++ src/cli/config_cli/label_cli/remove_cli.rs
@@ -0,0 +1,23 @@
+use clap::Parser;
+
+use cull_gmail::{Config, Error, Result};
+
+#[derive(Debug, Parser)]
+pub struct RemoveCli {
+    /// Id of the rule on which action applies
+    #[clap(short, long)]
+    id: usize,
+    /// Label to remove from the rule
+    #[clap(short, long)]
+    label: String,
+}
+
+impl RemoveCli {
+    pub fn run(&self, mut config: Config) -> Result<()> {
+        if config.get_rule(self.id).is_none() {
+            return Err(Error::RuleNotFound(self.id));
+        }
+
+        config.remove_label_from_rule(self.id, &self.label)
+    }
+}
