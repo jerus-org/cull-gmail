9674974ffe24977350705a69507a972aae94de9f
Jeremiah Russell <jerry@jrussell.ie>
2025-10-14 06:46:04 +0100

    ♻️ refactor(core): restructure modules for clarity
    - rename and relocate modules for better organization
    - replace `processor` with `rule_processor`
    - remove `delete` and `trash` modules

--- src/lib.rs
+++ src/lib.rs
@@ -6,31 +6,29 @@
 
 mod config;
 mod credential;
-mod delete;
 mod eol_action;
 mod error;
 mod gmail_client;
 mod message_list;
-mod processor;
 mod retention;
-mod trash;
+mod rule_processor;
 
 pub(crate) mod utils;
 
+pub use gmail_client::DEFAULT_MAX_RESULTS;
+
 pub use config::Config;
 pub use credential::Credential;
-pub use eol_action::EolAction;
-pub use error::Error;
-pub use gmail_client::DEFAULT_MAX_RESULTS;
 pub use gmail_client::GmailClient;
 pub(crate) use gmail_client::MessageSummary;
-pub use processor::RuleProcessor;
-pub use retention::MessageAge;
 pub use retention::Retention;
 
-pub use delete::Delete;
+pub use eol_action::EolAction;
+pub use error::Error;
+pub use retention::MessageAge;
+
 pub use message_list::MessageList;
-pub use trash::Trash;
+pub use rule_processor::RuleProcessor;
 
 /// Type alias for result with crate Error
 pub type Result<O> = std::result::Result<O, Error>;
