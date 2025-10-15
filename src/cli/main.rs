2fa7cd21ab7f41a0094ee22432a239a2ba671d00
Jeremiah Russell <jerry@jrussell.ie>
2025-10-15 14:26:21 +0100

    ♻️ refactor(rules): remove credentials config
    - remove unused credentials configuration
    - remove credentials file setting/getting logic
    - rename config file to rules.toml

--- src/cli/main.rs
+++ src/cli/main.rs
@@ -84,6 +84,15 @@ fn get_logging(level: log::LevelFilter) -> env_logger::Builder {
 }
 
 fn get_config() -> Result<Rules> {
+    // let settings = Config::builder()
+    //     // Add in `./Settings.toml`
+    //     .add_source(config::File::with_name("examples/simple/Settings"))
+    //     // Add in settings from the environment (with a prefix of APP)
+    //     // Eg.. `APP_DEBUG=1 ./target/app` would set the `debug` key
+    //     .add_source(config::Environment::with_prefix("APP"))
+    //     .build()
+    //     .unwrap();
+
     match Rules::load() {
         Ok(c) => Ok(c),
         Err(_) => {
