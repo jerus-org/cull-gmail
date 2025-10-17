6808f53b44258168cee59da3c0ad6990b2825ec8
Jeremiah Russell <jerry@jrussell.ie>
2025-10-17 17:22:28 +0100

    🐛 fix(build): correct readme generation script
    - fix readme generation script to point to the correct file

--- release-hook.sh
+++ release-hook.sh
@@ -4,7 +4,7 @@
 cat docs/readme/head.md > README.md
 # shellcheck disable=SC2129
 cat docs/main.md >> README.md
-cat docs/readme/lib.md >> README.md
+cat docs/lib.md >> README.md
 cat docs/readme/tail.md >> README.md   
 
 # Build Changelog
