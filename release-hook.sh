7b7a0d012d45f3c044541b92466464b47e3d1122
Jeremiah Russell <jerry@jrussell.ie>
2025-10-17 17:00:36 +0100

    📝 docs(readme): update readme content
    - fix readme generation script to include lib docs

--- release-hook.sh
+++ release-hook.sh
@@ -3,8 +3,8 @@
 # Build an updated README
 cat docs/readme/head.md > README.md
 # shellcheck disable=SC2129
-cat docs/lib.md >> README.md
 cat docs/main.md >> README.md
+cat docs/readme/lib.md >> README.md
 cat docs/readme/tail.md >> README.md   
 
 # Build Changelog
